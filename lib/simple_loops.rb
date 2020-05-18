cb69b6c42f96c8713f406f3d46e4de37b66ad6ab
eager-class-5814 <candacehchoe@gmail.com>
2020-05-18 02:07:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
-# Write your methods here
+def loop_message_five_times(string)
+counter = 0
+ 
+ while counter < 5
+   puts "Hello World.\n"
+   counter += 1
+ end
+end
+
+def loop_message_n_times(string, integer)
+  count = 0
+  
+   while count < 5
+   puts "Hello Moon.\n"
+
+   
+   while count < 10
+   puts "Hello Red Balloon.\n"
+   counter += 1
+ end
+ end
+ 
